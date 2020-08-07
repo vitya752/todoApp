a504ed7cdccd630bc46915a81a80425cfd5f2b1a
vitya752 <vitya602@gmail.com>
2020-08-08 02:49:19 +0300

    make it better

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -16,7 +16,7 @@ module.exports = async (req, res, next) => {
 
     try {
         const token = req.headers.authorization.split(' ')[1] || ""; //Bearer TOKEN
-
+        console.log(token)
         if(token.length > 0) {
             checkToken(req, res, token);
         }
