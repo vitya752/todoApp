7a07232add7f9a5b7daadb7c239eaf7590dd06b4
vitya752 <vitya602@gmail.com>
2020-08-08 01:49:57 +0300

    make it better

--- utils/checkToken.js
+++ utils/checkToken.js
@@ -3,13 +3,7 @@ const keys = require('./../keys');
 
 module.exports = (req, res, token) => {
     if(token) {
-        const decoded = jwt.verify(token, keys.JWT_SECRET);
-        const date = Date.now();
-
-        if(date > decoded.exp * 1000) {
-            return res.status(200).json({ message: 'Перезайдите пожалуйста...' });
-        }
-        req.user = decoded;
+        return true
     } else return res.status(200).json({ message: 'Перезайдите пожалуйста...' });
 
     return true;
