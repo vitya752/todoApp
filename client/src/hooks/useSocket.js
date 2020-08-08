6d44b619e96e3233869fe5aace329a9f1777d0e4
vitya752 <vitya602@gmail.com>
2020-08-08 03:01:41 +0300

    make it better

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import socketIOClient from "socket.io-client";
 
 const useSocket = () => {
-    return socketIOClient('http://localhost:5000');
+    return socketIOClient('https://minisocialnetw.herokuapp.com/');
 };
 
 export {
